6625e5dc15b51c93be15e42b363accf8f218ce06
Nghi Phan <nghi.phan@mrjaffesclass.com>
2023-04-25 11:44:29 -0700

    activity3-24

---
+++ activity3/__SHELL9.java
@@ -0,0 +1,16 @@
+package activity3;
+public class __SHELL9 extends bluej.runtime.Shell {
+public static java.lang.Object run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/home/student/Magpie/activity3");
+final activity3.Magpie3 magpie33 = (activity3.Magpie3)__bluej_runtime_scope.get("magpie33");
+
+
+java.lang.String __bluej_param0 = "Yesterday is today's day before";
+java.lang.String __bluej_param1 = "day";
+int __bluej_param2 = 0;
+try {
+return makeObj(magpie33.findKeyword(__bluej_param0,__bluej_param1,__bluej_param2)
+);}
+finally {
+}
+}}
